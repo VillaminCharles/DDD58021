61ac392cbd63847d38304fcf9be70f2b1807c1c1
Charles Villamin <98925514+VillaminCharles@users.noreply.github.com>
2023-03-21 12:21:36 +0800

    Add files via upload

--- Demo1.sql
+++ Demo1.sql
@@ -1,1 +1,4 @@
-SELECT * FROM testdatabase.orderstable;
+CREATE DATABASE testdatabase;
+CREATE TABLE orderstable(OrderID INT NOT NULL, OrderNumber INT NOT NULL, Primary KEY (OrderID));
+INSERT INTO orderstable(OrderID, OrderNumber)
+Values('1','77895'),('2','44678'),('3','22456');
